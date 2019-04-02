9a414f664dbe969aaadebb2ee9c079ef3bce0705
Paolo Pavlu <paolo@MacBook-Pro-di-Paolo.local>
2019-04-02 16:40:46 +0200

    restore PositionTest

---
+++ src/test/java/model/PositionTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PositionTest {
+    @Test
+    public void visibleTest() {/*
+        Position p1 = new Position(1, 2, 5, false, 0, false);
+        Position p2 = new Position(1, 1, 5, false, 0, false);
+        Position p3 = new Position(3, 4, 2, false, 0, false);
+        Position p4 = new Position(3, 2, 2, false, 0, false);
+
+        assertEquals(p1.visible(p2), true);
+        assertEquals(p2.visible(p1), true);
+        assertEquals(p1.visible(p3), false);
+        assertEquals(p3.visible(p1), false);
+        assertEquals(p3.visible(p4), true);
+        assertEquals(p4.visible(p3), true);*/
+    }
+}
