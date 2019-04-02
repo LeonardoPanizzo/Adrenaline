98e44af7119e7cc35b6f0a36be4eeec6887c83e8
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-04-02 21:45:56 +0200

    Update PositionTest.java

--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -7,10 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PositionTest {
     @Test
     public void visibleTest() {
-        Position p1 = new Position(1, 2, 5, false, 0, false);
-        Position p2 = new Position(1, 1, 5, false, 0, false);
-        Position p3 = new Position(3, 4, 2, false, 0, false);
-        Position p4 = new Position(3, 2, 2, false, 0, false);
+        Position p1 = new Position(1, 2, 5, false, false);
+        Position p2 = new Position(1, 1, 5, false, false);
+        Position p3 = new Position(3, 4, 2, false, false);
+        Position p4 = new Position(3, 2, 2, false, false);
 
         assertEquals(p1.visible(p2), true);
         assertEquals(p2.visible(p1), true);
