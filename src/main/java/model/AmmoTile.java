0d6eb9c45f014bd69da86ee55b53d0b62e019cd0
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-04-04 18:12:57 +0200

    Create AmmoTile.java

---
+++ src/main/java/model/AmmoTile.java
@@ -0,0 +1,16 @@
+package model;
+
+
+
+public class AmmoTile {
+    private char[] value;
+
+    public AmmoTile(){
+
+    }
+
+    //in UML avevamo messo che questo metodo restituisce un intero, non credo sia comodo lavorarci
+    public char[] getValue(){
+        return value;
+    }
+}
