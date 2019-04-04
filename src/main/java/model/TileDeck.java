1a2413ffb86aaf0bc4eb71234a4a9087a57dec47
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-04-04 18:13:01 +0200

    Create TileDeck.java

---
+++ src/main/java/model/TileDeck.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.Vector;
+
+public class TileDeck {
+    private int size;
+    private Vector<AmmoTile> AmmoDeck;
+
+    public TileDeck(){
+        size=0;
+    }
+
+    public AmmoTile pickUpAmmo(){
+        AmmoTile x=AmmoDeck.elementAt(size);
+        size++;
+        if(size>=AmmoDeck.capacity())       //se size raggiunge la dimensione del vettore lo azzero, posto il punatore al posto di spostare gli elementi dentro vector, bisognerà controllare il metodo capacity
+            size=0;
+        return x;
+    }
+}
