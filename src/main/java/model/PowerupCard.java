1d7f740f16c841821a854518e30c9c8690fad77c
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-04-09 16:31:00 +0200

    Update PowerupCard.java

--- src/main/java/model/PowerupCard.java
+++ src/main/java/model/PowerupCard.java
@@ -1,4 +1,11 @@
 package model;
 
 public class PowerupCard {
+    private char colour;
+    private String name;
+
+    public PowerupCard(String Name, char colour){
+        this.colour=colour;
+        this.name=name;
+    }
 }
