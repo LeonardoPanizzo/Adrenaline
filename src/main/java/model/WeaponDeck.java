fa0957be5195bd636dd44d55b31dad60c6fd950a
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-04-09 15:23:02 +0200

    Update WeaponDeck.java

--- src/main/java/model/WeaponDeck.java
+++ src/main/java/model/WeaponDeck.java
@@ -1,4 +1,25 @@
 package model;
 
+import java.util.Collections;
+import java.util.Vector;
+
 public class WeaponDeck {
+    private int size;
+    private Vector<WeaponCard> weaponDeck;
+
+    public WeaponDeck(){
+        size=0;
+    }
+
+    public void shuffle(){
+        Collections.shuffle(weaponDeck);
+    }
+
+    public WeaponCard pickUpWeapon(){
+        WeaponCard x=weaponDeck.elementAt(size);
+        size++;
+        if(size>=weaponDeck.capacity())       //se size raggiunge la dimensione del vettore lo azzero, sposto il punatore al posto di spostare gli elementi dentro vector, bisognerà controllare il metodo capacity
+            size=0;
+        return x;
+    }
 }
