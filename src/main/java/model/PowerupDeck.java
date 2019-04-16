d593aca297db177674d768e4fb898b18d69ca545
AndreaScinetti <10537247@polimi.it>
2019-04-16 18:23:37 +0200

    PowerupDeck added

--- src/main/java/model/PowerupDeck.java
+++ src/main/java/model/PowerupDeck.java
@@ -1,4 +1,25 @@
 package model;
 
+import java.util.Collections;
+import java.util.Vector;
+
 public class PowerupDeck {
+    private int size;
+    private Vector<PowerupCard> powerupDeck;
+
+    public PowerupDeck(){
+        size=0;
+    }
+
+    public void shuffle(){
+        Collections.shuffle(powerupDeck);
+    }
+
+    public PowerupCard pickUpPowerup(){
+        PowerupCard x=powerupDeck.elementAt(size);
+        size++;
+        if(size>=powerupDeck.capacity())       //se size raggiunge la dimensione del vettore lo azzero, sposto il punatore al posto di spostare gli elementi dentro vector, bisognerà controllare il metodo capacity
+            size=0;
+        return x;
+    }
 }
