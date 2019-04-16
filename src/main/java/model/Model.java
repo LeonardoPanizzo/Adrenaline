7a381a8ab101ee55e50e1cec31171d37c1f330d6
AndreaScinetti <10537247@polimi.it>
2019-04-16 18:22:25 +0200

    Model class added

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,4 +1,18 @@
 package model;
 
 public class Model {
+    private AmmoDeck ammoDeck;
+    private WeaponDeck weaponDeck;
+
+    public WeaponDeck getWeaponDeck() {
+        return weaponDeck;
+    }
+
+    public void setWeaponDeck(WeaponDeck weaponDeck) {
+        this.weaponDeck = weaponDeck;
+    }
+
+    public static void main (String args[]){
+
+    }
 }
