ae7afec211e05436b712d00d02956af935a2c069
AndreaScinetti <10537247@polimi.it>
2019-04-18 14:41:15 +0200

    Model Update

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,18 +1,31 @@
 package model;
 
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
 public class Model {
-    private AmmoDeck ammoDeck;
-    private WeaponDeck weaponDeck;
-
-    public WeaponDeck getWeaponDeck() {
-        return weaponDeck;
-    }
-
-    public void setWeaponDeck(WeaponDeck weaponDeck) {
-        this.weaponDeck = weaponDeck;
-    }
-
     public static void main (String args[]){
+        int numBoard;
+        System.out.println("Scegli il tabellone");
+        try{
+            numBoard = System.in.read();
+            Board board = new Board(numBoard);
+        }catch (IOException exception){exception.printStackTrace();};
 
+        Player players[] = new Player[5];
+        //todo: imostare time out per uscire dal loop e iniziare la partita
+        boolean timer = true;
+        while(timer){
+            System.out.println("Attendo giocatore...");
+            try{
+                int playerNumber = System.in.read();
+                players[playerNumber] = new Player (playerNumber);
+                if(playerNumber == 4)
+                    timer = false;
+            }catch (IOException exception){exception.printStackTrace();};
+        }
+
+        
     }
 }
