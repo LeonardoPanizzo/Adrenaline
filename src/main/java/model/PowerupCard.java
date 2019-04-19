31e680ebc8e6b65fa4caffd11fbfd78e86ab70dd
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-04-19 17:22:14 +0200

    Update PowerupCard.java

--- src/main/java/model/PowerupCard.java
+++ src/main/java/model/PowerupCard.java
@@ -3,7 +3,10 @@ package model;
 public class PowerupCard {
     protected char colour;
     protected String name;
-    protected Board board;
+    protected Board board; //serve il collegamento a board per poi poter accedere alle posizioni
+    //protected Player[] players; todo serve la lista dei giocatori per poter decidere su chi attivare gli effetti, se
+            //todo mettiamo la lista dei giocatori nella classe Board qui non ce n'è bisogno, penso sia più comodo
+            //todo averla nella classe board cosiì è piu semplice gestire i turni
 
     public PowerupCard(String name, char colour, Board b){
         this.name=name;
@@ -15,7 +18,7 @@ public class PowerupCard {
         return colour;
     }
 
-    public void activate(Player p){
+    public void activate(Player p){ //p is the player that just used the powerup
 
     }
 }
