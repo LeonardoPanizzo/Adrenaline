44536f34780a70ebd9336e26bbb6ed7be87d9f90
AndreaScinetti <10537247@polimi.it>
2019-04-19 21:29:21 +0200

    Model Updated

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -10,22 +10,32 @@ public class Model {
         System.out.println("Scegli il tabellone");
         try{
             numBoard = System.in.read();
-            Board board = new Board(numBoard);
-        }catch (IOException exception){exception.printStackTrace();};
+            Board board = new Board(numBoard);                          //Board creata
 
-        Player players[] = new Player[5];
-        //todo: imostare time out per uscire dal loop e iniziare la partita
-        boolean timer = true;
-        while(timer){
-            System.out.println("Attendo giocatore...");
-            try{
+            Player players[] = new Player[5];
+            //todo: imostare time out per uscire dal loop e iniziare la partita
+            boolean timer = true;
+            while(timer){
+                System.out.println("Attendo giocatore...");
                 int playerNumber = System.in.read();
                 players[playerNumber] = new Player (playerNumber);
                 if(playerNumber == 4)
                     timer = false;
-            }catch (IOException exception){exception.printStackTrace();};
-        }
+            }                                                           //Array di giocatori creato
 
-        
+            while(!board.isFinalRound()){
+                int i = 0;
+                board.setRound(i);
+
+                players[i].setRound(true);                              //todo dare il controllo al controller per le azioni del giocatore
+                if(players[i].isMadeDamage() != 0){
+                    for(int k : players[i].getDamagedPlayers()){
+               //         for(int y; y<)
+                        players[k].receivedDamages(i);
+                    }
+                }
+            }
+        }
+        catch (IOException exception){exception.printStackTrace();}
     }
 }
