cfd4b78c77a78602f3c02ca53f83226b172a60d6
AndreaScinetti <10537247@polimi.it>
2019-04-20 12:49:44 +0200

    Model Updated

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -23,15 +23,29 @@ public class Model {
                     timer = false;
             }                                                           //Array di giocatori creato
 
+            int courentPlayer = 0;
             while(!board.isFinalRound()){
-                int i = 0;
-                board.setRound(i);
+                courentPlayer = board.getRound() % 5;
+                while(players[courentPlayer] == null)                   //Needed when the players are not five
+                    courentPlayer = (courentPlayer + 1) % 5;
 
-                players[i].setRound(true);                              //todo dare il controllo al controller per le azioni del giocatore
-                if(players[i].isMadeDamage() != 0){
-                    for(int k : players[i].getDamagedPlayers()){
-               //         for(int y; y<)
-                        players[k].receivedDamages(i);
+                players[courentPlayer].setRound(true);                              //todo dare il controllo al controller per le azioni del giocatore
+                players[courentPlayer].setRound(false);                             //todo ridare il controllo al model
+
+                if(players[courentPlayer].getMadeDamage() != 0){                    //if the player makes damages, the method gives them to the other players.
+                    int [] damagedPlayers = players[courentPlayer].getDamagedPlayers();
+                    for(int k : damagedPlayers){
+                        for(int damage = 0; damage < players[courentPlayer].getMadeDamage(); damage++)
+                            players[damagedPlayers[k]].receivedDamages(courentPlayer);
+                        if(players[damagedPlayers[k]].getLife() == 0){
+                            board.setSkulls(damagedPlayers[k]);
+                        }
+                    }
+                }
+
+                for (int player = 0; player<5; player++){
+                    if (players[player].getLife() < 1){                 //when a player die, we update skulls in board
+
                     }
                 }
             }
