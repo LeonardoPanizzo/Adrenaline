cf94cf975e2e4e89ca48d17ecd03117e38ed647a
AndreaScinetti <10537247@polimi.it>
2019-04-24 13:33:28 +0200

    Updated WeaponCard

--- src/main/java/model/WeaponCard.java
+++ src/main/java/model/WeaponCard.java
@@ -1,5 +1,36 @@
 package model;
 
+import java.io.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+import java.io.IOException;
+import java.util.Iterator;
+
 public class WeaponCard {
 
+    private String name;
+
+    public WeaponCard(){
+        try{
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader("src/main/resources/" + this.name + ".json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File missing");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
