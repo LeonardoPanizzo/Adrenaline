b74304e45b5388b32eb87353682d73fb8129a27a
AndreaScinetti <10537247@polimi.it>
2019-04-29 14:25:09 +0200

    WeaponCard updated

--- src/main/java/model/WeaponCard.java
+++ src/main/java/model/WeaponCard.java
@@ -1,45 +1,16 @@
 package model;
 
-import java.io.*;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-
-import java.io.IOException;
-import java.util.Iterator;
-
 public class WeaponCard {
     protected String name;
     protected char[] costs;
     protected Board board;
     protected boolean loaded;
 
-    public WeaponCard(String name, char[] costs, Board b){
-        this.name=name;
-        this.costs=costs;
-        this.board=b;
-        loaded=true;
-        /*try{
-            JSONParser parser = new JSONParser();
-            Object obj = parser.parse(new FileReader("src/main/resources/WeaponCard.json"));
-
-            JSONObject jsonObject = (JSONObject) obj;
-
-
-        }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("File missing");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        leonardo: il file che viene letto per creare le carte ha più senso metterlo in weaponDeck che tiene il riferimento
-        di tutte le carte, non qui dove si lavora con una singola carta*/
+    public WeaponCard(String name, char[] costs, Board b) {
+        this.name = name;
+        this.costs = costs;
+        this.board = b;
+        loaded = true;
     }
 
     public void reload(){
