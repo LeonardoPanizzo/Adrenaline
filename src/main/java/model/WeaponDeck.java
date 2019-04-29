f59a0a4b9c131d14baa9b098a4ecc00acec2231f
AndreaScinetti <10537247@polimi.it>
2019-04-29 14:26:00 +0200

    WeaponDeck Updated

--- src/main/java/model/WeaponDeck.java
+++ src/main/java/model/WeaponDeck.java
@@ -3,6 +3,16 @@ package model;
 import java.util.Collections;
 import java.util.Vector;
 
+import java.io.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.Iterator;
+
 public class WeaponDeck {
     private int size;
     private Vector<WeaponCard> weaponDeck;
@@ -23,4 +33,23 @@ public class WeaponDeck {
             size=0;
         return x;
     }
+
+    /*try{
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader("src/main/resources/WeaponCard.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File missing");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+       */
+
 }
