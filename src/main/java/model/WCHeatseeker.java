afe0cf7eb535c47b944d2e281751fba06a966eec
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-05-02 22:27:36 +0200

    Update WCHeatseeker.java

--- src/main/java/model/WCHeatseeker.java
+++ src/main/java/model/WCHeatseeker.java
@@ -6,4 +6,15 @@ public class WCHeatseeker extends WeaponCard{
         super("HeatSeeker",new char[]{'r','r','y'},b);
 
     }
+
+    public boolean attack(Player attacker, int mode1, int[] mode2, Player[] attackedPlayers, Position[] movements){
+        if(this.isLoaded() && attackedPlayers.length==1 && !(attacker.getPosition().visible(attackedPlayers[0].getPosition()))){
+            for(int i=0;i<3;i++){
+                attackedPlayers[0].receivedDamages(attacker.getNumber());
+            }
+            loaded=false;
+            return true;
+        }
+        return false;
+    }
 }
