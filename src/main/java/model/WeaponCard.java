380f74d2ce3032c009f9c8a3b1bf0b704893ff52
AndreaScinetti <10537247@polimi.it>
2019-05-02 20:04:52 +0200

    Add Attack to WeaponCard

--- src/main/java/model/WeaponCard.java
+++ src/main/java/model/WeaponCard.java
@@ -5,6 +5,8 @@ public class WeaponCard {
     protected char[] costs;
     protected Board board;
     protected boolean loaded;
+    protected int damages;      //number of damages given
+    protected int marks;        //number of marks given
 
     public WeaponCard(String name, char[] costs, Board b) {
         this.name = name;
@@ -22,6 +24,14 @@ public class WeaponCard {
         for(int i=1; i<=4; i++)
             temp[i]=costs[i];
         return temp;
+    }
+
+    public int[] attack(Player attacker, int mode1, int[] mode2, Player[] attackedPlayers, Position[] movements){
+        //todo implementare il metodo
+        //todo tenere traccia dei danni assegnati ai giocatori (serve per sapere quante volte chiamare receivedDamage) e dei marchi assegnati
+        //todo: l'ideale sarebbe restituire un arraya di due valori: danni fatti e maechi assegnati.
+        int[] damagesAndMarks = {this.damages, this.marks};
+        return damagesAndMarks;
     }
 
     public char[] getCostReloading(){
