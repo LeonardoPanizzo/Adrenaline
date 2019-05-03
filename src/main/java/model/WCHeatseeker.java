1ee71c7492013d636742122e72da0c88f7c7b64a
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-05-03 19:21:58 +0200

    Update WCHeatseeker.java

--- src/main/java/model/WCHeatseeker.java
+++ src/main/java/model/WCHeatseeker.java
@@ -1,12 +1,19 @@
 package model;
 
+/**
+ * effect: Choose 1 target you cannot see and deal 3 damage to it.
+ * Notes: Yes, this can only hit targets you cannot see
+ */
 public class WCHeatseeker extends WeaponCard{
 
-    public WCHeatseeker(Board b){
-        super("HeatSeeker",new char[]{'r','r','y'},b);
+    public WCHeatseeker(){
+        super("HeatSeeker",new char[]{'r','r','y'});
 
     }
-    //andre: penso vada aggiunto qui: @Override
+    @Override
+    /*
+    if checks that there is only one target,that the weapon is loaded and that the target isn't visible from the attacker
+     */
     public boolean attack(Player attacker, int mode1, int[] mode2, Player[] attackedPlayers, Position[] movements){
         if(this.isLoaded() && attackedPlayers.length==1 && !(attacker.getPosition().visible(attackedPlayers[0].getPosition()))){
             for(int i=0;i<3;i++){
