6551af671173f5cbfb805aca2e27fe90d60200cb
AndreaScinetti <10537247@polimi.it>
2019-05-03 08:59:29 +0200

    WCHeatseeker comment added

--- src/main/java/model/WCHeatseeker.java
+++ src/main/java/model/WCHeatseeker.java
@@ -6,7 +6,7 @@ public class WCHeatseeker extends WeaponCard{
         super("HeatSeeker",new char[]{'r','r','y'},b);
 
     }
-
+    //andre: penso vada aggiunto qui: @Override
     public boolean attack(Player attacker, int mode1, int[] mode2, Player[] attackedPlayers, Position[] movements){
         if(this.isLoaded() && attackedPlayers.length==1 && !(attacker.getPosition().visible(attackedPlayers[0].getPosition()))){
             for(int i=0;i<3;i++){
