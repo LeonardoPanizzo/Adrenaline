b76611b82d36ad603dc8771b7e21c0d73358034c
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-05-07 19:19:55 +0200

    Update WCLockRifle.java

--- src/main/java/Adrenaline/model/WCLockRifle.java
+++ src/main/java/Adrenaline/model/WCLockRifle.java
@@ -19,15 +19,19 @@ public class WCLockRifle extends WeaponCard{
             if(mode2.length==1){//checks if it is only the basic effect
                 for(int i=0;i<2;i++)
                     attackedPlayers[0].receivedDamages(attacker);
-                    //dare un marcgio a attacked[0]
+                attackedPlayers[0].setMarksReceived(attacker,1);
+                attacker.setMarksGiven(attackedPlayers[0],1);
+                }
                 done=true;
-            }else if(attackedPlayers.length==2 && mode2.length==2 &&((mode2[0]==0 && mode2[1]==1)||(mode2[0]==1 &&mode2[1]==0))){ //checks if there is the basic effect and the optional one
-                   if(attacker.getPosition().visible(attackedPlayers[1].getPosition())){//checks if the second player is visible
-                       for(int i=0;i<2;i++)
-                           attackedPlayers[0].receivedDamages(attacker);
-                       //dare un marchio a attacked[0] e uno a attacked [1]
-                       done=true;
-                   }
+            }else if(attackedPlayers.length==2 && mode2.length==2 &&((mode2[0]==0 && mode2[1]==1)||(mode2[0]==1 &&mode2[1]==0))) { //checks if there is the basic effect and the optional one
+            if (attacker.getPosition().visible(attackedPlayers[1].getPosition())) {//checks if the second player is visible
+                for (int i = 0; i < 2; i++)
+                    attackedPlayers[0].receivedDamages(attacker);
+                attackedPlayers[0].setMarksReceived(attacker, 1);
+                attacker.setMarksGiven(attackedPlayers[0], 1);
+                attackedPlayers[1].setMarksReceived(attacker, 1);
+                attacker.setMarksGiven(attackedPlayers[1], 1);
+                done = true;
             }
         }
         return done;
