12b767fa817bbf04070f57e3572c341f1b2ab3be
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-05-07 19:20:20 +0200

    Update PowerupCard.java

--- src/main/java/Adrenaline/model/PowerupCard.java
+++ src/main/java/Adrenaline/model/PowerupCard.java
@@ -3,10 +3,7 @@ package Adrenaline.model;
 public class PowerupCard {
     protected char colour;
     protected String name;
-    protected Board board; //serve il collegamento a board per poi poter accedere alle posizioni
-    //protected Player[] players; todo serve la lista dei giocatori per poter decidere su chi attivare gli effetti, se
-    //todo mettiamo la lista dei giocatori nella classe Board qui non ce n'è bisogno, penso sia più comodo
-    //todo averla nella classe board cosiì è piu semplice gestire i turni
+    protected Board board;
 
     public PowerupCard(String name, char colour, Board b){
         this.name=name;
