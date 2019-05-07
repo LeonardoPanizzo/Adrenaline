5f532691abf1b0f69e0cd4ada90575adffa2a419
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-05-07 19:19:47 +0200

    Update PowerupDeck.java

--- src/main/java/Adrenaline/model/PowerupDeck.java
+++ src/main/java/Adrenaline/model/PowerupDeck.java
@@ -5,21 +5,41 @@ import java.util.Vector;
 
 public class PowerupDeck {
     private int size;
-    private Vector<PowerupCard> powerupDeck;
+    private Vector<PowerupCard> PUDeck;
 
-    public PowerupDeck(){
+    public PowerupDeck(Board board){
         size=0;
+        PUDeck=new Vector<PowerupCard>(24);
+        for(int i=0;i<2;i++){//create for each power up two copies with the same colour
+            PUDeck.add(new PUNewton('b', board));
+            PUDeck.add(new PUNewton('r', board));
+            PUDeck.add(new PUNewton('y', board));
+
+            PUDeck.add(new PUTagbackGrenade('b', board));
+            PUDeck.add(new PUTagbackGrenade('r', board));
+            PUDeck.add(new PUTagbackGrenade('y', board));
+
+            PUDeck.add(new PUTargetingScope('b', board));
+            PUDeck.add(new PUTargetingScope('r', board));
+            PUDeck.add(new PUTargetingScope('y', board));
+
+            PUDeck.add(new PUTeleporter('b', board));
+            PUDeck.add(new PUTeleporter('r', board));
+            PUDeck.add(new PUTeleporter('y', board));
+        }
+        shuffle();
     }
 
     public void shuffle(){
-        Collections.shuffle(powerupDeck);
+        Collections.shuffle(PUDeck);
     }
 
     public PowerupCard pickUpPowerup(){
-        PowerupCard x=powerupDeck.elementAt(size);
+        PowerupCard x=PUDeck.elementAt(size);
         size++;
-        if(size>=powerupDeck.capacity())       //se size raggiunge la dimensione del vettore lo azzero, sposto il punatore al posto di spostare gli elementi dentro vector, bisognerà controllare il metodo capacity
+        if(size>=24)    //the deck contains 24 cards
             size=0;
+        //todo aggiungere il rimescolare il mazzo
         return x;
     }
 }
