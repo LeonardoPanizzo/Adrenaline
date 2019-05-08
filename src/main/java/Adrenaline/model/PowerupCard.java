c9fbc444ca71a6cc7197804927524f30df0b4df9
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-05-08 18:31:37 +0200

    Update PowerupCard.java

--- src/main/java/Adrenaline/model/PowerupCard.java
+++ src/main/java/Adrenaline/model/PowerupCard.java
@@ -19,7 +19,7 @@ public class PowerupCard {
         return name;
     }
 
-    public boolean use(Player p1, Player p2, Position[] squares){
+    public boolean use(Player p1, Player p2, Position[] squares, char payment){
         return false;
     }
 }
