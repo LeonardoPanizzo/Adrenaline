f05500ff3492b5e45f7b9709baee9d0b1be4afa5
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-05-10 16:49:25 +0200

    Update WeaponDeck.java

--- src/main/java/Adrenaline/model/WeaponDeck.java
+++ src/main/java/Adrenaline/model/WeaponDeck.java
@@ -6,10 +6,13 @@ import java.util.Vector;
 public class WeaponDeck {
     private int size;
     private Vector<WeaponCard> weaponDeck;
-    //todo inserirò le carte qui dentro, nel costruttore
 
     public WeaponDeck(){
         size=0;
+        weaponDeck=new Vector<WeaponCard>(21);
+        weaponDeck.add(new WCLockRifle());
+        weaponDeck.add(new WCHeatseeker());
+        shuffle();
     }
 
     public void shuffle(){
@@ -19,7 +22,7 @@ public class WeaponDeck {
     public WeaponCard pickUpWeapon(){
         WeaponCard x=weaponDeck.elementAt(size);
         size++;
-        if(size>=weaponDeck.capacity()) {       //se size raggiunge la dimensione del vettore lo azzero, sposto il punatore al posto di spostare gli elementi dentro vector, bisognerà controllare il metodo capacity
+        if(size>=21) {       //se size raggiunge la dimensione del vettore lo azzero, sposto il punatore al posto di spostare gli elementi dentro vector, bisognerà controllare il metodo capacity
             size = 0;
             shuffle();
         }
