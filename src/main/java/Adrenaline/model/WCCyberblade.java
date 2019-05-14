adb572c56dc278f6e91800faa7acb2a8015944d1
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-05-14 11:53:03 +0200

    Update WCCyberblade.java

--- src/main/java/Adrenaline/model/WCCyberblade.java
+++ src/main/java/Adrenaline/model/WCCyberblade.java
@@ -19,6 +19,63 @@ public class WCCyberblade extends WeaponCard{
     @Override
     public boolean attack(Player attacker, int mode1, int[] mode2, Player[] attackedPlayers, Position[] movements, PowerupCard[] payment){
         boolean done=false;
+        if(isLoaded() && attackedPlayers.length<=2 && attackedPlayers.length>=1){
+            Position attackerpos=attacker.getPosition();
+            Position attackedpos=attackedPlayers[0].getPosition();
+            if(mode2.length==1 && mode2[0]==0 && attackerpos.equals(attackedpos)){
+                for(int i=0; i<2; i++)
+                    attackedPlayers[0].receivedDamages(attacker);
+                loaded=false;
+                done=true;
+            }else if(mode2.length==2){
+                if(mode2[0]==0 && mode2[1]==1 && movements.length==1 && attackerpos.equals(attackedpos) && attackerpos.reachable(movements[0])){
+                    for(int i=0; i<2; i++)
+                        attackedPlayers[0].receivedDamages(attacker);
+                    attacker.setPosition(movements[0]);
+                    loaded=false;
+                    done=true;
+                }else if(mode2[0]==1 && mode2[1]==0 && movements.length==1 && movements[0].equals(attackedpos) && attackerpos.reachable(movements[0])){
+                    attacker.setPosition(movements[0]);
+                    for(int i=0; i<2; i++)
+                        attackedPlayers[0].receivedDamages(attacker);
+                    loaded=false;
+                    done=true;
+                }else if(mode2[0]==0 && mode2[1]==2 && attackedPlayers.length==2 && attackerpos.equals(attackedpos) && attackerpos.equals(attackedPlayers[1].getPosition()) && isPaid(attacker, payment, mode2)){
+                    for(int i=0; i<2; i++) {
+                        attackedPlayers[0].receivedDamages(attacker);
+                        attackedPlayers[1].receivedDamages(attacker);
+                    }
+                    loaded=false;
+                    done=true;
+                }
+            }else if(mode2.length==3 && attackedPlayers.length==2 && movements.length==1){
+                if(mode2[0]==0 && mode2[1]==1 && mode2[2]==2 && attackerpos.equals(attackedpos) && attackerpos.reachable(movements[0]) && movements[0].equals(attackedPlayers[0].getPosition()) && isPaid(attacker, payment, mode2)){
+                    for(int i=0; i<2; i++)
+                        attackedPlayers[0].receivedDamages(attacker);
+                    attacker.setPosition(movements[0]);
+                    for(int i=0; i<2; i++)
+                        attackedPlayers[1].receivedDamages(attacker);
+                    loaded=false;
+                    done=true;
+                }else if(mode2[0]==1 && mode2[1]==0 && mode2[2]==2 && attackerpos.reachable(movements[0]) && movements[0].equals(attackedpos) && attackerpos.equals(attackedPlayers[1].getPosition()) && isPaid(attacker, payment, mode2)){
+                    attacker.setPosition(movements[0]);
+                    for(int i=0; i<2; i++)
+                        attackedPlayers[0].receivedDamages(attacker);
+                    for(int i=0; i<2; i++)
+                        attackedPlayers[1].receivedDamages(attacker);
+                    loaded=false;
+                    done=true;
+                }else if(mode2[0]==0 && mode2[1]==2 && mode2[2]==1 && attackerpos.reachable(movements[0]) && attackerpos.equals(attackedpos) && attackerpos.equals(attackedPlayers[1].getPosition()) && isPaid(attacker, payment, mode2)){
+                    for(int i=0; i<2; i++)
+                        attackedPlayers[0].receivedDamages(attacker);
+                    for(int i=0; i<2; i++)
+                        attackedPlayers[1].receivedDamages(attacker);
+                    attacker.setPosition(movements[0]);
+                    loaded=false;
+                    done=true;
+                }
+            }
+        }
         return done;
     }
 }
