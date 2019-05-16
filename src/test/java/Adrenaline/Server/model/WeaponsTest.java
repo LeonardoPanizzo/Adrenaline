2a029bff4563a47ea2d46f64e5b844f3bf4d0b15
AndreaScinetti <10537247@polimi.it>
2019-05-16 20:15:20 +0200

    Cyberblade test completed

--- src/test/java/Adrenaline/Server/model/WeaponsTest.java
+++ src/test/java/Adrenaline/Server/model/WeaponsTest.java
@@ -77,6 +77,59 @@ public class WeaponsTest {
         assertEquals(3, p1.getLife(), "Shot goes wrong");
         assertEquals(9, p2.getLife(), "Shot goes wrong");
 
-        //mode2 = {0,, 1, 2}
+        //mode2 = {0, 1, 2}, {1, 0, 2}, {1, 2, 0}
+        p0.setPosition(b.getBoard()[0][2]);
+        p1.setPosition(b.getBoard()[0][2]);
+        p2.setPosition(b.getBoard()[1][2]);
+        p0.setAmmo('r', 1);
+        p0.setAmmo('y', 2);
+        ammoSel = new char[] {'r', 'y'};
+        p0.reload(weapon, ammoSel);
+        p0.setAction(2);
+        mode2 = new int[] {0,1,2};
+        posArray = new Position[]{b.getBoard()[1][2]};
+        p0.shot(weapon, players, -1, mode2, posArray, null);
+
+        assertEquals(1, p1.getLife(), "Shot goes wrong");
+        assertEquals(7, p2.getLife(), "Shot goes wrong");
+
+        p1 = new Player(1, pd);
+        p0.setPosition(b.getBoard()[1][2]);
+        p1.setFirstPosition(b.getBoard()[0][2]);
+        p2.setPosition(b.getBoard()[0][2]);
+        p0.setAmmo('r', 1);
+        p0.setAmmo('y', 2);
+        ammoSel = new char[] {'r', 'y'};
+        p0.reload(weapon, ammoSel);
+        p0.setAction(2);
+        mode2 = new int[] {1, 0, 2};
+        posArray = new Position[]{b.getBoard()[0][2]};
+        players = new Player[] {p1, p2};
+        p0.shot(weapon, players, -1, mode2, posArray, null);
+
+        assertEquals(9, p1.getLife(), "Shot goes wrong");
+        assertEquals(5, p2.getLife(), "Shot goes wrong");
+
+        p0.setFirstPosition(b.getBoard()[0][2]);
+        p1.setFirstPosition(b.getBoard()[0][2]);
+        p2.setFirstPosition(b.getBoard()[0][2]);
+        p0.setAmmo('r', 1);
+        p0.setAmmo('y', 1);
+        ammoSel = new char[] {'r', 'y'};
+        p0.reload(weapon, ammoSel);
+        p0.setAction(2);
+        mode2 = new int[] {0, 2, 1};
+        posArray = new Position[]{b.getBoard()[1][2]};
+        players = new Player[] {p1, p2};
+        PowerupCard[] power = {new PowerupCard("Power", 'y')};
+        p0.setPowerup(power);
+
+        assertNotNull(power[0]);
+        assertEquals('y', power[0].getColour());
+
+        p0.shot(weapon, players, -1, mode2, posArray, power);
+
+        assertEquals(7, p1.getLife(), "Shot goes wrong");
+        assertEquals(3, p2.getLife(), "Shot goes wrong");
     }
 }
