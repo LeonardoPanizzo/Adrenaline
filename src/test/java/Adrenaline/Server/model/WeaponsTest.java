e72b67dec2c6aa83932ab93c551bd1d45311271f
AndreaScinetti <10537247@polimi.it>
2019-05-18 11:14:30 +0200

    Hellion test completed

--- src/test/java/Adrenaline/Server/model/WeaponsTest.java
+++ src/test/java/Adrenaline/Server/model/WeaponsTest.java
@@ -132,4 +132,56 @@ public class WeaponsTest {
         assertEquals(7, p1.getLife(), "Shot goes wrong");
         assertEquals(3, p2.getLife(), "Shot goes wrong");
     }
+
+    @Test
+    public void HellionTest(){
+        Board b = new Board(2);
+        PowerupDeck pd = new PowerupDeck();
+        Player p0 = new Player(0, pd);
+        p0.setAction(2);
+        Player p1 = new Player(1, pd);
+        Player p2 = new Player(2, pd);
+        Player p3 = new Player(3, pd);
+        p0.setFirstPosition(b.getBoard()[0][2]);
+        p1.setFirstPosition(b.getBoard()[0][3]);
+        p2.setFirstPosition(b.getBoard()[0][3]);
+        p3.setFirstPosition(b.getBoard()[1][3]);
+        Position pos = b.getBoard()[0][2];
+        WeaponCard weapon = new WCHellion();
+        pos.chooseArm(0);
+        pos.chooseArm(1);
+        pos.chooseArm(2);
+        pos.giveWeapon(weapon);
+        char[] ammoSel = new char[] {'y'};
+        Player[] players = {p1};
+        PowerupCard[] payment = new PowerupCard[]{new PowerupCard("Power", 'r')};
+        p0.setPowerup(payment);
+        p0.grabWeaponCard(weapon,ammoSel);
+
+        //mode1 = 0
+        p0.shot(weapon, players, 0, null, null, payment);
+
+        assertEquals(10, p1.getLife(), "p1 life isn't correct");
+        assertEquals(1, p1.getMarksReceived()[0], "p1 marks aren't correct");
+        assertEquals(1, p2.getMarksReceived()[0], "p2 marks aren't correct");
+        assertEquals(0, p3.getMarksReceived()[0], "p3 marks aren't correct");
+        int[] marks = new int[]{0, 1, 1, 0, 0};
+        assertArrayEquals(marks, p0.getMarksGiven(), "p0 marks given are not correct");
+
+        //Mode1 = 1
+        p0.setAmmo('r', 1);
+        p0.setAmmo('y', 1);
+        p0.setAction(2);
+        char[]selAmmo = new char[]{'r', 'y'};
+        p0.reload(weapon, selAmmo);
+
+        p0.shot(weapon, players, 1, null, null, payment);
+
+        assertEquals(8, p1.getLife(), "p1 life isn't correct");
+        assertEquals(2, p1.getMarksReceived()[0], "p1 marks aren't correct");
+        assertEquals(3, p2.getMarksReceived()[0], "p2 marks aren't correct");
+        assertEquals(0, p3.getMarksReceived()[0], "p3 marks aren't correct");
+        marks = new int[]{0, 2, 3, 0, 0};
+        assertArrayEquals(marks, p0.getMarksGiven(), "p0 marks given are not correct");
+    }
 }
