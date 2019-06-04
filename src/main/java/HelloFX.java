95b5966e328fd8f950659ce02168654f8b0e383b
AndreaScinetti <10537247@polimi.it>
2019-06-04 18:08:41 +0200

    Pom problem

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -8,8 +8,8 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
+       String javaVersion = "java.version";
+       String javafxVersion = "javafx.version";
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
