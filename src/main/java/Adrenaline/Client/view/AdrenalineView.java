5c26d6e2e4e3a3ba2ba38dfc015c12c726b2795a
AndreaScinetti <10537247@polimi.it>
2019-06-11 16:27:17 +0200

    GUI tests

--- src/main/java/Adrenaline/Client/view/AdrenalineView.java
+++ src/main/java/Adrenaline/Client/view/AdrenalineView.java
@@ -11,6 +11,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -19,6 +22,9 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class AdrenalineView extends Application {
 
     public static void main(String[] args) {
@@ -26,7 +32,7 @@ public class AdrenalineView extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start (Stage primaryStage) throws FileNotFoundException {
         primaryStage.setTitle("Adrenaline");
 
         //Setting the login gridPane
@@ -37,19 +43,20 @@ public class AdrenalineView extends Application {
         grid.setPadding(new Insets(25, 25, 25, 25));
 
         Text sceneTitle = new Text("Welcome!");
-        sceneTitle.setFont(Font.font("Times New Roman", FontWeight.NORMAL, 30));
+        sceneTitle.setFont(Font.font("Times New Roman", FontWeight.BOLD, 30));
         sceneTitle.setFill(Color.FIREBRICK);
+//        Image image = new Image(new FileInputStream("src\\main\\resources\\Title.png"));
+//        ImageView imageView = new ImageView(image);
+
         grid.add(sceneTitle, 1, 0, 2, 1);
         Label userName = new Label( "User Name: ");
+        userName.setFont(Font.font("Times New Roman", FontWeight.BOLD, 11));
+
         grid.add(userName, 0, 2);
         TextField userTextField = new TextField();
         grid.add(userTextField, 1, 2);
-        Label pw = new Label("Password: ");
-        grid.add(pw, 0, 3);
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1,3);
 
-        Button btn = new Button("Login");
+        Button btn = new Button("Start");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btn);
@@ -62,11 +69,12 @@ public class AdrenalineView extends Application {
             @Override
             public void handle(ActionEvent e) {
                 actionTarget.setFill(Color.FIREBRICK);
-                actionTarget.setText("Pulsante Login premuto");
+                actionTarget.setText("Game is starting...");
+
             }
         });
 
-        Scene scene = new Scene(grid, 300, 275);
+        Scene scene = new Scene(grid, 800, 500);
         primaryStage.setScene(scene);
         scene.getStylesheets().add
                 (AdrenalineView.class.getResource("/view.css").toExternalForm());
