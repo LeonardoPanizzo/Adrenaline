e6e32a3213ea8a5901c2e6471e72f71c92dd7599
AndreaScinetti <10537247@polimi.it>
2019-06-15 20:58:37 +0200

    GUI improved

--- src/main/java/Adrenaline/Client/view/AdrenalineView.java
+++ src/main/java/Adrenaline/Client/view/AdrenalineView.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
@@ -19,6 +20,7 @@ import javafx.scene.image.*;
 public class AdrenalineView extends Application {
 
     String name = new String();
+    int count = 0;
 
     public static void main(String[] args) {
         launch(args);
@@ -54,11 +56,19 @@ public class AdrenalineView extends Application {
         Button btn = new Button("Play");
         username.getChildren().add(btn);
 
+        primaryStage.setScene(myScene);
+        primaryStage.show();
+
+        rootNode = new StackPane();
+        Scene secondScene = new Scene(rootNode, 1000, 600);
+
+
         user.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent ae) {
                 name = user.getText();
-                System.out.println("The name is: "+name);
+                //System.out.println("The name is: "+name);
+                primaryStage.setScene(secondScene);
             }
         });
 
@@ -66,11 +76,68 @@ public class AdrenalineView extends Application {
             @Override
             public void handle(ActionEvent actionEvent) {
                 name = user.getText();
-                System.out.println("The name is: "+name);
+                //System.out.println("The name is: "+name);
+                primaryStage.setScene(secondScene);
             }
         });
 
-        primaryStage.setScene(myScene);
-        primaryStage.show();
+        //todo:creare sala d'attesa: impostare che il nome dei giocatori compare
+        VBox lobby = new VBox(10);
+        Label player1 = new Label("Player 1");
+        player1.setTextFill(Color.YELLOW);
+        player1.setFont(Font.font("", FontWeight.BOLD, 20));
+        Label player2 = new Label("Player 2");
+        player2.setTextFill(Color.GRAY);
+        player2.setFont(Font.font("", FontWeight.BOLD, 20));
+        Label player3 = new Label("Player 3");
+        player3.setTextFill(Color.GREEN);
+        player3.setFont(Font.font("", FontWeight.BOLD, 20));
+        Label player4 = new Label("Player 4");
+        player4.setTextFill(Color.VIOLET);
+        player4.setFont(Font.font("", FontWeight.BOLD, 20));
+        Label player5 = new Label("Player 5");
+        player5.setTextFill(Color.BLUE);
+        player5.setFont(Font.font("", FontWeight.BOLD, 20));
+        lobby.setAlignment(Pos.CENTER);
+        lobby.getChildren().addAll(player1,player2,player3,player4,player5);
+        Image lobbyBack = new Image(AdrenalineView.class.getResource("/lobbyback.jpg").toExternalForm());
+        ImageView lobbybackground = new ImageView(lobbyBack);
+        Label wait = new Label("Waiting for other players...");
+        wait.setFont(Font.font("", FontWeight.BOLD, 20));
+        wait.setTextFill(Color.WHITE);
+        FlowPane waiting = new FlowPane(10,10);
+        waiting.setAlignment(Pos.BOTTOM_RIGHT);
+        waiting.getChildren().add(wait);
+        rootNode.getChildren().add(lobbybackground);
+        rootNode.getChildren().addAll(lobby, waiting);
+
+        rootNode = new StackPane();
+        Scene boardScene = new Scene(rootNode, 1000, 600);
+
+        //Todo: quando il timer scade si passa alla schermata di gioco
+        //si va avanti temporaneamente con click del mouse
+        secondScene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                count += me.getClickCount();
+                System.out.println("Count is "+count);
+                if(count == 1)
+                    wait.setText("Click to Play");
+                else if(count > 1)
+                    primaryStage.setScene(boardScene);
+            }
+        });
+
+        GridPane playerView = new GridPane();
+        Image x0y0 = new Image(AdrenalineView.class.getResource("/0-0.png").toExternalForm());
+        ImageView X0Y0 = new ImageView(x0y0);
+        StackPane xx0yy0 = new StackPane(X0Y0);
+        Image x0y1 = new Image(AdrenalineView.class.getResource("/0-1.png").toExternalForm());
+        ImageView X0Y1 = new ImageView(x0y1);
+        StackPane xx0yy1 = new StackPane(X0Y1);
+        playerView.add(xx0yy0, 0,0);
+        playerView.add(xx0yy1, 1,0);
+
+        rootNode.getChildren().add(playerView);
     }
 }
