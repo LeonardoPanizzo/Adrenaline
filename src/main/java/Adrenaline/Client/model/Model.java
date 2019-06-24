b0230591a6e1282efb1a49d026d1c66465544ee4
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-06-24 19:51:25 +0200

    Create Model.java

---
+++ src/main/java/Adrenaline/Client/model/Model.java
@@ -0,0 +1,60 @@
+package Adrenaline.Client.model;
+import Adrenaline.Server.model.Board;
+import Adrenaline.Server.model.Player;
+import Adrenaline.Server.model.PowerupCard;
+
+import java.util.Scanner;
+
+public class Model{
+    private Board board;
+    Player player;
+    Player[] players;
+
+    /**
+     * Action is a char and not an integer so the system is more robust
+     * @return
+     */
+    public char chooseAction(){
+        char action;
+        Scanner keyboard=new Scanner(System.in);
+        do {
+            System.out.println("choose what action you want to do \n0. print information\n1.move\n2.pick up\n3.shoot\n4.use powerup");
+            action=keyboard.next().charAt(0);
+        }while(action>='0' || action<='4');
+        return action;
+    }
+
+    public void updateInformation(Board b, Player p, Player[] ps){
+        this.board=b;
+        this.player=p;
+        this.players=ps;
+    }
+
+    /**
+     * created to avoid cloned code
+     * @return
+     */
+    private boolean hasThisPowerup(String x){
+        PowerupCard[] power;
+        power=player.getPowerup();
+        boolean hasone=false;
+        for(int i=0;i<power.length && !hasone; i++){
+            hasone=power[i].getName().equals(x);
+        }
+        return hasone;
+    }
+
+    public boolean hasTargetingScope(){
+        return hasThisPowerup("targeting scope");
+    }
+
+    public boolean hasTagnackGrenade(){
+        return hasThisPowerup("tagback grenade");
+    }
+
+    public static void main (String args[]) {
+        //todo parte in cui viene stabilita la connessione
+
+
+    }
+}
