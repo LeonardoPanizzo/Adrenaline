f9ee49494a68351b415d291b7414b17100891744
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-06-27 22:35:21 +0200

    Update Clientj.java

--- src/main/java/Adrenaline/Clientj.java
+++ src/main/java/Adrenaline/Clientj.java
@@ -1,6 +1,8 @@
 package Adrenaline;
 
 import Adrenaline.Server.model.Board;
+import Adrenaline.Server.model.Info;
+import Adrenaline.Server.model.Player;
 
 import java.io.*;
 import java.net.InetAddress;
@@ -8,15 +10,85 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Clientj {
+    private static Board board;
+    private static Player[] players;
+    private static Player player;
 
 
     public static void fromServer(Socket s){
-
         try {
             ObjectInputStream inStream = new ObjectInputStream(s.getInputStream());
             Board board = (Board) inStream.readObject();
             System.out.println("Object received = " + board);
 
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void printInfo(){
+        System.out.println(board.getRound());
+        System.out.println(player.completeString());//information about themselves
+        for(int i=0; i<players.length; i++){        //information about other players
+            System.out.println(players[i].toString());
+        }
+    }
+
+    public static void setmovement(){//todo: vedere se passare come parametro il numero di passi che è possibile fare
+        //todo:obtain the positions and then send them to the server
+    }
+
+    public static void pickup(){
+        //todo:prendere i movimenti
+        //server.pickup(player.getnumber());
+    }
+
+    public static void shoot(){
+        //todo:obtain the information for the attack, per i parametri non passati impostare oggetti a null, se i comandi sono sbalgiati si torna nel metodo chooseaction
+    }
+
+    public static void usePowerUp(){
+        //todo: selezionare il powerup da usare e fare il controllo se è un powerup che si può usare in quel momento
+        //todo: poi impostare i parametri non utilizzati da quel powerup con oggettti null
+    }
+
+
+    public static void chooseAction(){
+        char action;
+        Scanner keyboard=new Scanner(System.in);
+        do {
+            System.out.println("choose what action you want to do \n0. print information\n1.move\n2.pick up\n3.shoot\n4.use powerup");
+            action=keyboard.next().charAt(0);
+            if(action>='0' && action<='4'){
+                switch (action){
+                    case '0':
+                        printInfo();
+                        break;
+                    case '1':
+                        setmovement();
+                        break;
+                    case '2':
+                        pickup();
+                        break;
+                    case '3':
+                        shoot();
+                        break;
+                    case '4':
+                        usePowerUp();
+                        break;
+                }
+            }
+        }while(action<'0' || action>'4');//todo:aggiungere anche il controllo sul numero player.action
+    }
+
+    public static void upodateInfo(Socket s){
+        try {
+            ObjectInputStream inStream = new ObjectInputStream(s.getInputStream());
+            Info info=(Info) inStream.readObject();
+            board=info.getBoard();
+            
         } catch (IOException e){
             e.printStackTrace();
         } catch (ClassNotFoundException e){
