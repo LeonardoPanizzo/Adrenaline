51d3c22227738f29f2055cf5be55b163a10437ee
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-06-27 22:35:24 +0200

    Update Model.java

--- src/main/java/Adrenaline/Server/model/Model.java
+++ src/main/java/Adrenaline/Server/model/Model.java
@@ -1,24 +1,6 @@
 package Adrenaline.Server.model;
 
-import Adrenaline.Server.control.BiController;
-
 import java.util.Scanner;
-import Adrenaline.Server.model.Response;
-import Adrenaline.ClientHandler;
-import Adrenaline.Server.model.Board;
-import Adrenaline.Server.model.PowerupDeck;
-import Adrenaline.Server.model.RequestHandler;
-import Adrenaline.Server.model.commands.BoardResponse;
-import Adrenaline.Server.model.commands.CreateBoardRequest;
-import Adrenaline.Server.model.commands.CreatePUDeckRequest;
-import Adrenaline.Server.model.commands.PUDeckResponse;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.Vector;
-import java.rmi.RemoteException;
 
 public class Model{//todo: dovrebbero gli attributi essere static?
     private static Board board;
@@ -54,14 +36,13 @@ public class Model{//todo: dovrebbero gli attributi essere static?
         }
     }
 
-
-    public static void main (String args[]) {
-        //todo parte in cui viene stabilita la connessione
-        //todo: la gestione dei turni, alla fine del turno
-        board.setFinalRound();
-        board.isFinalRound();//usato per vedere la fine dei turni clasici quindi finisce il ciclo e poi si passa al turno finale
+    public Board getBoard(){
+        return board;
     }
 
+    public Player[] getPlayers(){
+        return players;
+    }
 
         /*
         //todo stampe necessarie a inizio turno per il giocatore in CLI
@@ -206,3 +187,121 @@ public class Model{//todo: dovrebbero gli attributi essere static?
     */
 
 }
+
+
+//todo qui c'è la prima versione del model del client
+/*
+package Adrenaline.Client.model;
+import Adrenaline.Server.model.Board;
+import Adrenaline.Server.model.Player;
+import Adrenaline.Server.model.PowerupCard;
+import Adrenaline.Server.model.WeaponCard;
+
+import java.util.Scanner;
+
+public class Model{
+    private Board board;
+    private Player player;
+    private Player[] players;
+
+    /**
+     * Action is a char and not an integer so the system is more robust
+     * @return
+     */
+/*
+public void chooseAction(){
+    char action;
+    Scanner keyboard=new Scanner(System.in);
+    do {
+        System.out.println("choose what action you want to do \n0. print information\n1.move\n2.pick up\n3.shoot\n4.use powerup");
+        action=keyboard.next().charAt(0);
+        if(action>='0' && action<='4'){
+            switch (action){
+                case '0':
+                    printInfo();
+                    break;
+                case '1':
+                    setmovement();
+                    break;
+                case '2':
+                    pickup();
+                    break;
+                case '3':
+                    shoot();
+                    break;
+                case '4':
+                    usePowerUp();
+                    break;
+            }
+        }
+    }while(action<'0' || action>'4');//todo:aggiungere anche il controllo sul numero player.action
+}
+
+    public void printInfo(){
+        System.out.println(board.getRound());
+        System.out.println(player.completeString());//information about themselves
+        for(int i=0; i<players.length; i++){        //information about other players
+            System.out.println(players[i].toString());
+        }
+    }
+
+    public void setmovement(){//todo: vedere se passare come parametro il numero di passi che è possibile fare
+        //todo:obtain the positions and then send them to the server
+    }
+
+    public void pickup(){
+        //todo:prendere i movimenti
+        //server.pickup(player.getnumber());
+    }
+
+    public void shoot(){
+        //todo:obtain the information for the attack, per i parametri non passati impostare oggetti a null, se i comandi sono sbalgiati si torna nel metodo chooseaction
+    }
+
+    public void usePowerUp(){
+        //todo: selezionare il powerup da usare e fare il controllo se è un powerup che si può usare in quel momento
+        //todo: poi impostare i parametri non utilizzati da quel powerup con oggettti null
+    }
+
+    public void selectreload(){
+        WeaponCard[] weapons=player.getWeapons();
+        //      PowerupCard[] useammo=
+    }
+
+    public void updateInformation(Board b, Player p, Player[] ps){
+        this.board=b;
+        this.player=p;
+        this.players=ps;
+    }
+
+    /**
+     * created to avoid cloned code
+     * Used to see if a player has a specific powerup, the one used before attaccking and the one after you received an attack
+     * @return
+     */
+/*
+    private boolean hasThisPowerup(String x){
+        PowerupCard[] power;
+        power=player.getPowerup();
+        boolean hasone=false;
+        for(int i=0;i<power.length && !hasone; i++){
+            hasone=power[i].getName().equals(x);
+        }
+        return hasone;
+    }
+
+    public boolean hasTargetingScope(){
+        return hasThisPowerup("targeting scope");
+    }
+
+    public boolean hasTagnackGrenade(){
+        return hasThisPowerup("tagback grenade");
+    }
+
+    public static void main (String args[]) {
+        //todo parte in cui viene stabilita la connessione
+
+    }
+}
+
+ */
