5a9e4108c1c7bfb1e9834b7cd26053ae717e449e
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-07-03 01:38:45 +0200

    Update RemoteBiCon.java

--- src/main/java/Adrenaline/Server/control/RemoteBiCon.java
+++ src/main/java/Adrenaline/Server/control/RemoteBiCon.java
@@ -1,8 +1,6 @@
 package Adrenaline.Server.control;
 
-import Adrenaline.Server.model.Board;
-import Adrenaline.Server.model.Player;
-import Adrenaline.Server.model.Response;
+import Adrenaline.Server.model.*;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -22,9 +20,9 @@ public interface RemoteBiCon extends Remote {
 
     void createBoard(Integer boardNumber) throws RemoteException;
 
-    void setBoard()throws RemoteException;
+    //void setBoard()throws RemoteException;
 
-    void setBoard(int b) throws RemoteException;
+
 
     void spreadinfo(int specificuser)throws RemoteException;
 
@@ -36,15 +34,15 @@ public interface RemoteBiCon extends Remote {
 
     int getOnetogo()throws RemoteException;
 
-    void movefinal2(int playernumber)throws RemoteException;
+    //void movefinal2(int playernumber)throws RemoteException;
 
-    void moveandgrabfinal1(int playernumber)throws RemoteException;
+    //void moveandgrabfinal1(int playernumber)throws RemoteException;
 
-    void moveandgrabfinal2(int playernumber)throws RemoteException;
+    //void moveandgrabfinal2(int playernumber)throws RemoteException;
 
-    void attackfinal2(int playernumber)throws RemoteException;
+    //void attackfinal2(int playernumber)throws RemoteException;
 
-    void attackfinal1(int playernumber)throws RemoteException;
+    //void attackfinal1(int playernumber)throws RemoteException;
 
     int finalplayernumber()throws RemoteException;
 
@@ -62,23 +60,53 @@ public interface RemoteBiCon extends Remote {
 
     void setRespawnturn(int specificuser)throws RemoteException;
 
-    void useTagbackGrenade(int specificuser)throws RemoteException;
+    //void useTagbackGrenade(int specificuser)throws RemoteException;
 
-    void move(int playernumber)throws RemoteException;
+    //void move(int playernumber)throws RemoteException;
 
-    void moveandgrab(int playernumber)throws RemoteException;
+    //void moveandgrab(int playernumber)throws RemoteException;
 
-    void attack(int playernumber)throws RemoteException;
+    //void attack(int playernumber)throws RemoteException;
 
-    void usePowerup(int playernumber)throws RemoteException;
+    //void usePowerup(int playernumber)throws RemoteException;
 
-    void reload(int playernumber)throws RemoteException;
+    //void reload(int playernumber)throws RemoteException;
 
     void endturn(int playernumber)throws RemoteException;
 
     int getNumber()throws RemoteException;
 
-    void respawn(int playernumber)throws RemoteException;
+    //void respawn(int playernumber)throws RemoteException;
+
+    //todo:after this final interface
+
+    void reload(int specificuser, WeaponCard weapontoreaload, PowerupCard[] payment)throws RemoteException;
+
+    void reload(int specificuser, WeaponCard weapontoreaload)throws RemoteException;
+
+    boolean shoot(int specificuser, WeaponCard weapon,Player[] toattack,int mode1, int[]mode2,Position[] pos,PowerupCard[] payment)throws RemoteException;
+
+    void usePowerup(int specificuser, PowerupCard pwr, Position[] pos,char a)throws RemoteException;
+
+    void useTagBackGrenade(int specificuser, PowerupCard pwr,char a)throws RemoteException;
+
+    boolean grabWeapon(int specificuser, WeaponCard weapon)throws RemoteException;
+
+    boolean grabWeapon(int specificuser, WeaponCard weapon, PowerupCard[] payment)throws RemoteException;
+
+    void discardWeapon(int specificuser, WeaponCard weapon)throws RemoteException;
+
+    void addWeapon(int specificuser, WeaponCard weapon)throws RemoteException;
+
+    void weaponToGround(Position p, WeaponCard weapon)throws RemoteException;
+
+    boolean grabAmmo(int specificuser) throws RemoteException;
+
+    boolean respawn(int specificuser, PowerupCard pwr, Position pos) throws RemoteException;
+
+    void setBoard(int b) throws RemoteException;
+
+    boolean moveplayer(int specificuser, boolean after, Position[] movements) throws RemoteException;
 
     /*
     void createBoard(Integer boardNumber) throws RemoteException;
