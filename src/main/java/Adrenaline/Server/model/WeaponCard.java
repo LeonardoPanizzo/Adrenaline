646e8c10337ed9c59b608f1552d88becf0fb0f56
AndreaScinetti <10537247@polimi.it>
2019-07-04 14:41:54 +0200

    JavaDoc is added

--- src/main/java/Adrenaline/Server/model/WeaponCard.java
+++ src/main/java/Adrenaline/Server/model/WeaponCard.java
@@ -10,6 +10,14 @@ public class WeaponCard implements Serializable {
     protected boolean loaded;
 
 
+    /**
+     * WeaponCard's constructor. A weapon card has a name, a cost, the cost for each effect a boolean attribute to show if has an optional fire mode or not.
+     *
+     * @param name name of the weapon
+     * @param costs cost to reload the weapon
+     * @param costEffects cost to use effects
+     * @param has true if it has an optional fire mode
+     */
     public WeaponCard(String name, char[] costs, char[] costEffects, boolean has) {
         this.name = name;
         this.costs = costs;
@@ -18,14 +26,27 @@ public class WeaponCard implements Serializable {
         loaded = true;
     }
 
+    /**
+     * Method to take the card's name.
+     *
+     * @return the cards name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Method to reload the weapon.
+     */
     public void reload(){
         this.loaded=true;
     }
 
+    /**
+     * Method to take the cost to take this from a position that's a respawn point.
+     *
+     * @return the cost to take this
+     */
     public char[] getCostTaking(){
         char[] temp=new char[costs.length-1];
         for(int i=1; i<costs.length; i++)
@@ -33,23 +54,47 @@ public class WeaponCard implements Serializable {
         return temp;
     }
 
+    /**
+     * Method to know if the card has an optional fire mode or not.
+     *
+     * @return true if this has an optional fire mode
+     */
     public boolean hasoptional(){
         return hasoptionaleffect;
     }
 
+    /**
+     * Method to use the weapon. It uses the player who attacks, the fire mode (1 or 2 and each combo), the player or
+     * the players who/whose are attacked, positions where move attacker, attacked or other effects. It also use powerup
+     * to pay the ammo cost of the optional effects.
+     *
+     * @param attacker player who use the weapon
+     * @param mode1 select the base mode o the alternative fire
+     * @param mode2 select the optional mode and the sequence how they will be used
+     * @param attackedPlayers player that will be damaged
+     * @param movements position where move attacker, attacked or  position used for other effects
+     * @param payment power up cards to eventually pay the cost of optional effects
+     * @return
+     */
     public boolean attack(Player attacker, int mode1, int[] mode2, Player[] attackedPlayers, Position[] movements, PowerupCard[] payment){
-        //viene chiamato il metodo delle sottoclassi
+        //later it is called from each weapon sub-class
         return false;
     }
 
+    /**
+     * Method to obtain the weapon's cost to reload it
+     *
+     * @return reloading cost
+     */
     public char[] getCostReloading(){
-        return costs.clone();           //andrea: altrimenti si ritorna l'attributo e può essere modificato!
+        return costs.clone();
     }
 
     /**
-     * give one damage to all the players in movements, used only by WCFurnace and WCShockwave
-     * @param attacker
-     * @param movements
+     * give one damage to all the players in movements, used only by WCFurnace and WCShockwave.
+     *
+     * @param attacker the player who attacks
+     * @param movements positions where players are damaged
      */
     protected void attackall(Player attacker, Position[] movements){
         Player[] temp;
@@ -61,7 +106,15 @@ public class WeaponCard implements Serializable {
         }
     }
 
-    public boolean isPaid(Player p, PowerupCard[] payment){ //valid only for the weapon with only an addiotional effect or only one option
+    /**
+     * Method valid only for the weapon with only an additional effect or only one option. It controls if this
+     * additional effect or the option is payed.
+     *
+     * @param p player who has ammo
+     * @param payment power up cards are used to pay the ammo cost
+     * @return
+     */
+    public boolean isPaid(Player p, PowerupCard[] payment){
         boolean paid=false;
         if(payment == null || payment.length==0){
             paid=p.updateAmmo(costEffects);
@@ -97,6 +150,14 @@ public class WeaponCard implements Serializable {
         return paid;
     }
 
+    /**
+     * Method to control if all optional effects are payed by the player.
+     *
+     * @param p player who pays
+     * @param payment power up cards are used to pay the ammo cost
+     * @param mode mode that needs a payment
+     * @return
+     */
     public boolean isPaid(Player p, PowerupCard[] payment, int[] mode){
         boolean paid=false;
         int[] temporaryCost=new int[3];
@@ -133,12 +194,20 @@ public class WeaponCard implements Serializable {
         return paid;
     }
 
-
-
+    /**
+     * Method to see if a weapon is loaded or not.
+     *
+     * @return true if the weapon is loaded
+     */
     public boolean isLoaded() {
         return loaded;
     }
 
+    /**
+     * Method to set loaded a weapon. It is used to reload a weapon.
+     *
+     * @param loaded set to true if the weapon is loaded
+     */
     public void setLoaded(boolean loaded) {
         this.loaded = loaded;
     }
