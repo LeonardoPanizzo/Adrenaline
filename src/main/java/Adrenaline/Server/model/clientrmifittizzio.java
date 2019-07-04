a544da13d002486cd263cf1500026bdbef5218c3
LeonardoPanizzo <48493158+LeonardoPanizzo@users.noreply.github.com>
2019-07-05 00:35:59 +0200

    Delete clientrmifittizzio.java

--- src/main/java/Adrenaline/Server/model/clientrmifittizzio.java
+++
@@ -1,9 +0,0 @@
-package Adrenaline.Server.model;
-
-public class clientrmifittizzio {
-    /*
-    //todo:fare la parte del primo turno in cui si posizione il giocatore
-    fare il controllo se è turno del giocatore
-
-     */
-}
